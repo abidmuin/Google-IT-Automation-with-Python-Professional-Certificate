9a27b3abc2f0fa8a948d3732cf5cf49666eb45a0
muin2473 <muin.739@gmail.com>
2020-04-08 20:49:14 +0600

    Create network.py

---
+++ 2. Using Python to Interact with the Operating System/Week 1/network.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+import requests
+import socket
+
+
+def check_localhost():
+    localhost = socket.gethostbyname('localhost')
+    if(localhost == "127.0.0.1"):
+        return True
+
+
+def check_connectivity():
+    request = requests.get("http://www.google.com")
+    if(request.status_code == 200):
+        return True
