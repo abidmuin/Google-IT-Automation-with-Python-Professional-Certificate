0226d158ccc8af926ab7d93a30c3a66b273a2641
muin2473 <muin.739@gmail.com>
2020-04-13 13:01:43 +0600

    Create generate_report.py

---
+++ 2. Using Python to Interact with the Operating System/Week 2/generate_report.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+import csv
+
+
+def read_employees(csv_file_location):
+    empDialect = csv.register_dialect(
+        'empDialect', skipinitialspace=True, strict=True)
+    employee_file = csv.DictReader(
+        open(csv_file_location), dialect='empDialect')
+    employee_list = []
+    for data in employee_file:
+        employee_list.append(data)
+        return employee_list
+
+
+def process_data(employee_list):
+    department_list = []
+    for employee_data in employee_list:
+        department_list.append(employee_data['Department'])
+        department_data = {}
+        for department_name in set(department_list):
+            department_data[department_name] = department_list.count(
+                department_name)
+            return department_data
+
+
+def write_report(dictionary, report_file):
+    with open(report_file, "w+") as f:
+        for k in sorted(dictionary):
+            f.write(str(k) + ':' + str(dictionary[k]) + '\n')
+        f.close()
+
+
+employee_list = read_employees(
+    '/home/student-00-ebb75c51d543/data/employees.csv')
+dictionary = process_data(employee_list)
+write_report(dictionary, '/home/student-00-ebb75c51d543/test_report.txt')
+
+# print(dictionary)
+
+# print(employee_list)
