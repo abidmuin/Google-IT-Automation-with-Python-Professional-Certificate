f319166b0d58f47010dba2ecf76f69fd5d19d702
muin2473 <muin.739@gmail.com>
2020-04-16 19:27:33 +0600

    Update Practice Quizes.py

--- 2. Using Python to Interact with the Operating System/Week 3/Practice Quizes.py
+++ 2. Using Python to Interact with the Operating System/Week 3/Practice Quizes.py
@@ -84,9 +84,12 @@ international format, with "+1-" in front of the phone number.
 Fill in the regular expression, using groups, to use the transform_record function to do that.
 '''
 
-
+'''
 def transform_record(record):
-    new_record = re.sub(___)
+    #re.sub(pattern, replace, string)
+    pattern = ','
+    replace = ',+1-'
+    new_record = re.sub(pattern, replace, record, 1)
     return new_record
 
 
@@ -101,3 +104,66 @@ print(transform_record("Melody Daniels,846-687-7436,Programmer"))
 
 print(transform_record("Charlie Rivera,698-746-3357,Web Developer"))
 # Charlie Rivera,+1-698-746-3357,Web Developer
+'''
+
+'''
+4. The multi_vowel_words function returns all words with 3 
+or more consecutive vowels (a, e, i, o, u). 
+
+Fill in the regular expression to do that.
+
+'''
+
+'''
+def multi_vowel_words(text):
+    pattern = r"\w*[aeiou]{3,}\w*"
+    result = re.findall(pattern, text)
+    return result
+
+
+print(multi_vowel_words("Life is beautiful"))
+# ['beautiful']
+
+print(multi_vowel_words("Obviously, the queen is courageous and gracious."))
+# ['Obviously', 'queen', 'courageous', 'gracious']
+
+print(multi_vowel_words(
+    "The rambunctious children had to sit quietly and await their delicious dinner."))
+# ['rambunctious', 'quietly', 'delicious']
+
+print(multi_vowel_words("The order of a data queue is First In First Out (FIFO)"))
+# ['queue']
+
+print(multi_vowel_words("Hello world!"))
+# []
+'''
+
+'''
+The transform_comments function converts comments in a Python script into those usable by a C compiler. 
+This means looking for text that begins with a hash mark (#) and replacing it with double slashes (//), 
+which is the C single-line comment indicator. For the purpose of this exercise, 
+we'll ignore the possibility of a hash mark embedded inside of a Python command, 
+and assume that it's only used to indicate a comment. 
+We also want to treat repetitive hash marks (##), (###), etc., as a single comment indicator, 
+to be replaced with just (//) and not (#//) or (//#). 
+
+Fill in the parameters of the substitution method to complete this function:
+'''
+
+'''
+def transform_comments(line_of_code):
+    pattern = '\#{1,}'
+    replace = '//'
+    result = re.sub(pattern, replace, line_of_code)
+    return result
+
+
+print(transform_comments("### Start of program"))
+# Should be "// Start of program"
+print(transform_comments("  number = 0   ## Initialize the variable"))
+# Should be "  number = 0   // Initialize the variable"
+print(transform_comments("  number += 1   # Increment the variable"))
+# Should be "  number += 1   // Increment the variable"
+print(transform_comments("  return(number)"))
+# Should be "  return(number)"
+'''
