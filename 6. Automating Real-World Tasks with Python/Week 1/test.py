e1380e7a4e22bb7d17a9f2e18884f7d629667868
muin2473 <muin.739@gmail.com>
2020-04-25 19:33:27 +0600

    Update test.py

--- 6. Automating Real-World Tasks with Python/Week 1/test.py
+++ 6. Automating Real-World Tasks with Python/Week 1/test.py
@@ -1,14 +1,20 @@
 #!/usr/bin/env python3
 
+import glob
+import os
+from PIL import Image
 import os.path
 from os import path
 
-def main():
-	print ("Is it File?" + str(path.isfile('/home/n1ghtmar3/Desktop/Qwiklabs/Week_1/images/ic_add_location_black_48dp')))
-	print ("Is it File?" + str(path.isdir('/home/n1ghtmar3/Desktop/Qwiklabs/Week_1/images/ic_add_location_black_48dp')))
 
-if __name__== "__main__":
-	main()
+def main():
+    print("Is it File?" + str(path.isfile(
+        '/home/n1ghtmar3/Desktop/Qwiklabs/Week_1/images/ic_add_location_black_48dp')))
+    print("Is it File?" + str(path.isdir('/home/n1ghtmar3/Desktop/Qwiklabs/Week_1/images/ic_add_location_black_48dp')))
+
+
+if __name__ == "__main__":
+    main()
 
 """
 path = "/root/Desktop/python/images/"
@@ -25,37 +31,34 @@ def resize():
 resize()
 """
 
-#Another one 
+# Another one
 
 #!/usr/bin/env python3
 
-from PIL import Image
-import os
-import glob
-
 
 def convert():
-  print("Inside")
+    print("Inside")
 
-  for item in glob.glob("ic_*"):
-    print(item)
-    img = Image.open(item).convert('RGB')
-    print("Check")
-    img.rotate(-90).resize((128,128)).save("/opt/icons/" + item, "JPEG"),
-    print("Success!")
+    for item in glob.glob("ic_*"):
+        print(item)
+        img = Image.open(item).convert('RGB')
+        print("Check")
+        img.rotate(-90).resize((128, 128)).save("/opt/icons/" + item, "JPEG"),
+        print("Success!")
 
 
 if __name__ == "__main__":
-  convert()
+    convert()
 
 
 #################################################
-    if infile[0] == ".":
-        continue
-    im = Image.open(source_dir + infile)
-    out = im.rotate(90).resize((128,128)).convert("RGBA")
-    background = Image.new('RGBA', out.size, (255,255,255))
-    final = Image.alpha_composite(background, out).convert("RGB")
-    new_file = new_dir + infile
-    final.save(new_file, 'JPEG', quality=80)
-
+    """
+        if infile[0] == ".":
+            continue
+        im = Image.open(source_dir + infile)
+        out = im.rotate(90).resize((128,128)).convert("RGBA")
+        background = Image.new('RGBA', out.size, (255,255,255))
+        final = Image.alpha_composite(background, out).convert("RGB")
+        new_file = new_dir + infile
+        final.save(new_file, 'JPEG', quality=80)
+    """
