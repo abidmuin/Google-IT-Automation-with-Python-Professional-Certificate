81c86c17b2a3078f10b643c12ce5cb84e4f6ccbf
muin2473 <muin.739@gmail.com>
2020-05-01 14:42:36 +0600

    Update example.py

--- 6. Automating Real-World Tasks with Python/Week 3/scripts/example.py
+++ 6. Automating Real-World Tasks with Python/Week 3/scripts/example.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python3
-
 import emails
 import os
 import reports
-
 table_data = [
     ['Name', 'Amount', 'Value'],
     ['elderberries', 10, 0.45],
@@ -12,14 +10,13 @@ table_data = [
     ['durians', 1, 25],
     ['bananas', 5, 1.99],
     ['cherries', 23, 5.80],
-    ['grapes', 13, 2.48]]
+    ['grapes', 13, 2.48],
+    ['kiwi', 4, 0.49]]
 reports.generate("/tmp/report.pdf", "A Complete Inventory of My Fruit",
                  "This is all my fruit.", table_data)
-
-sender = "sender@example.com"
+sender = "automation@example.com"
 receiver = "{}@example.com".format(os.environ.get('USER'))
 subject = "List of Fruits"
 body = "Hi\n\nI'm sending an attachment with all my fruit."
-
 message = emails.generate(sender, receiver, subject, body, "/tmp/report.pdf")
 emails.send(message)
